7d94788e1aafc3dca030f89accd93a8112ef81a9
Agata Polak <polakagata16@gmail.com>
2025-10-21 21:27:59 +0200

    Sterowanie postacią

--- src/main/java/edu/io/Main.java
+++ src/main/java/edu/io/Main.java
@@ -1,8 +1,12 @@
 package edu.io;
 
+import java.util.Scanner;
 import edu.io.token.GoldToken;
 import edu.io.token.PlayerToken;
 import edu.io.token.Token;
+
+import static java.nio.file.Files.move;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +14,40 @@ public class Main {
         Board board = new Board();
         board.placeToken(1,3,new GoldToken());
         PlayerToken player = new PlayerToken(board, 0,0);
+
+        Scanner scanner = new Scanner(System.in);
+
         board.display();
+
+        while(true){
+
+
+            try{
+                switch(scanner.nextLine().strip().toUpperCase()){
+                    case "W": player.move(PlayerToken.Move.UP);
+                        break;
+
+                    case "A": player.move(PlayerToken.Move.LEFT);
+                        break;
+
+                    case "S": player.move(PlayerToken.Move.DOWN);
+                        break;
+
+                    case "D": player.move(PlayerToken.Move.RIGHT);
+                        break;
+                }
+                board.display();
+            }catch (Exception e) {
+                System.err.println("Cannot move outside the board");
+            }
+        }
+
+
+
+
+
+        }
+
+
     }
-}
+
